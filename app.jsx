8baa3faffdab8d6a4dad71c6572777be86d2b357
Jua2905 <juanmaberdun29@gmail.com>
2025-09-05 15:21:28 +0000

    Primeros pasos

---
+++ app.jsx
@@ -0,0 +1,21 @@
+import './app.css'
+
+function app() {
+    
+    const numero = 10 + 5;
+
+    const usuario={
+        nombre : 'Juan Manuel',
+         
+    }
+
+   retun (
+         <>
+             <h1>WELCOME to my new PAGE REACT JS</h1>
+             <p>{usuario}</p>
+             <p>{numero}</p>
+         </>
+    )
+}
+
+export default app
